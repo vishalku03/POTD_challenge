1bce7707de2b0e31afe69fa6a71579473e698ee5
vishalk0002 <vishalk000123@gmail.com>
2024-06-13 18:49:03 +0530

    leetcode

---
+++ day_22/minimum number to seat everyone.cpp
@@ -0,0 +1,17 @@
+
+class Solution
+{
+public:
+     int minMovesToSeat(vector<int> &seats, vector<int> &students)
+     {
+          sort(seats.begin(), seats.end());
+          sort(students.begin(), students.end());
+
+          int ans = 0;
+          for (int i = 0; i < students.size(); i++)
+          {
+               ans = ans + abs(students[i] - seats[i]);
+          }
+          return ans;
+     }
+};
