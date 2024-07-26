dd6622cd6d501cd75833b35da45f9def75939624
vishalk0002 <vishalk000123@gmail.com>
2024-07-26 18:14:36 +0530

    fd

---
+++ day_65/k  pangrams.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+     bool kPangram(string str, int k)
+     {
+          // code here
+          unordered_map<char, int> m;
+          int count = 0;
+          for (int i = 0; i < str.size(); i++)
+          {
+               if (str[i] != ' ')
+               {
+                    count++;
+                    m.insert({str[i], 1});
+               }
+          }
+          return (26 - m.size() <= count - m.size() && 26 - m.size() <= k) ? true : false;
+     }
+};
+
+// gfg
