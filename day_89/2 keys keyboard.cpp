01f7d3c46f4204d287a9e5d8e353a2ddc5af7712
vishalk0002 <vishalk000123@gmail.com>
2024-08-19 12:07:35 +0530

    lec

---
+++ day_89/2 keys keyboard.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        
+        if(n==1)  return 0;
+        int ans =  0;
+        int i = 2;
+        while(i<=n)
+        {
+            if(n%i==0)
+            {
+                ans =  ans+i;
+                n = n/i;
+            }
+            else
+            i++;
+        }
+
+        return ans;
+    }
+};
+
+// leetcode -  650
