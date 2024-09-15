8ff5189bde3e2ddd5f325ac485948741c8aa7243
vishalk0002 <vishalk000123@gmail.com>
2024-09-15 19:35:46 +0530

    lc

---
+++ day_116/Find the longest substring comtaining vowels im even words.cpp
@@ -0,0 +1,27 @@
+//Approach-2 (using Mask and Xor property)
+class Solution {
+public:
+    int findTheLongestSubstring(string s) {
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        int mask = 0;
+        int maxL = 0;
+        for(int i = 0; i<s.length(); i++) {
+            int val = 0;
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+                val = s[i]; //agar vowel hai to uski value lelo else consonant me 0 rahegi val
+            
+            mask = mask^val; //xor nikala
+            
+            if(!mp.count(mask)) //agar past me nahi dekha to map me daaldo
+                mp[mask] = i;
+            
+            maxL = max(maxL, i - mp[mask]); //maxL nikaal lo
+        }
+        return maxL;
+    }
+};
+
+// leetcode -  1371
+
+
