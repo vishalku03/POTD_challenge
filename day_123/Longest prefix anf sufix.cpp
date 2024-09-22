fca04b51fc5e7526d1b9b89eba9b61a936644f16
vishalk0002 <vishalk000123@gmail.com>
2024-09-22 15:05:28 +0530

    gfg

---
+++ day_123/Longest prefix anf sufix.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+     int lps(string str)
+     {
+          // code here
+          int n = str.length();
+          int i = 0, k = 0, j = 1, c = 0;
+          while (i < n && j < n)
+          {
+               if (str[i] == str[j])
+               {
+                    i++;
+                    j++;
+                    c++;
+               }
+               else
+               {
+                    i = 0;
+                    k++;
+                    j = k;
+                    c = 0;
+               }
+          }
+
+          return c;
+     }
+};
+// gfg
