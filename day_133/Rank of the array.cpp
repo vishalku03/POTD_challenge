e57f41cae67ed471563e5dd9e218fd8568bd219b
vishalk0002 <vishalk000123@gmail.com>
2024-10-02 18:05:18 +0530

    lc

---
+++ day_133/Rank of the array.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+public:
+     vector<int> arrayRankTransform(vector<int> &arr)
+     {
+
+          vector<int> temp = arr;
+          sort(temp.begin(), temp.end());
+          auto it = unique(temp.begin(), temp.end());
+          temp.erase(it, temp.end());
+          unordered_map<int, int> mp;
+          for (int i = 0; i < temp.size(); i++)
+          {
+               mp[temp[i]] = i + 1;
+          }
+          vector<int> ans;
+          for (auto i : arr)
+          {
+               ans.push_back(mp[i]);
+          }
+
+          return ans;
+     }
+};
+
+/*
+
+create a copy of the original array
+sort the temporary array
+remove  suplicates
+Asssign ranks to each unique element
+Replace elements in the original array with their ranks
+
+*/
+
+
+// leetcode -  1331
