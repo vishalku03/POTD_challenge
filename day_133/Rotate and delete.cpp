e883b3bb9efa64168f73d977b54c63c8c011c055
vishalk0002 <vishalk000123@gmail.com>
2024-10-02 23:44:58 +0530

    leetcode

--- day_133/Rotate and delete.cpp
+++ day_133/Rotate and delete.cpp
@@ -1,35 +1,36 @@
-class Solution
-{
-public:
-     int rotateDelete(vector<int> &arr)
-     {
-          int z = arr.size();
-          int operations = z / 2;
+// class Solution
+// {
+// public:
+//      int rotateDelete(vector<int> &arr)
+//      {
+//           int z = arr.size();
+//           int operations = z / 2;
 
-          for (int k = 1; k <= operations; k++)
-          {
-               rotate(arr);
+//           for (int k = 1; k <= operations; k++)
+//           {
+//                rotate(arr);
 
-               int n = arr.size();
-               int pos = n - k;
-               arr.erase(arr.begin() + pos);
-          }
+//                int n = arr.size();
+//                int pos = n - k;
+//                arr.erase(arr.begin() + pos);
+//           }
 
-          return arr[0];
-     }
+//           return arr[0];
+//      }
 
-     void rotate(vector<int> &arr)
-     {
-          int n = arr.size();
-          int last = arr[n - 1];
+//      void rotate(vector<int> &arr)
+//      {
+//           int n = arr.size();
+//           int last = arr[n - 1];
 
-          for (int i = n - 1; i > 0; i--)
-          {
-               arr[i] = arr[i - 1];
-          }
+//           for (int i = n - 1; i > 0; i--)
+//           {
+//                arr[i] = arr[i - 1];
+//           }
 
-          arr[0] = last;
-     }
-};
+//           arr[0] = last;
+//      }
+// };
 
-// gfg
+// // gfg
+
