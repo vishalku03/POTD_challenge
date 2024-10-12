5b3eb371f2d966c741e63dd2f4cc0eaa891e476c
vishalk0002 <vishalk000123@gmail.com>
2024-10-12 06:34:55 +0530

    gfg

---
+++ day_143/Two smallest in the every subaary.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+     int pairWithMaxSum(vector<int> &arr)
+     {
+
+          if (arr.size() == 1)
+               return -1;
+
+          int n = arr.size();
+          int i = 0;
+          int j = 1;
+          int sum = 0;
+          while (j < n)
+          {
+               sum = max(sum, arr[i] + arr[j]);
+               i++;
+               j++;
+          }
+
+          return sum;
+     }
+};
+
+// gfg
