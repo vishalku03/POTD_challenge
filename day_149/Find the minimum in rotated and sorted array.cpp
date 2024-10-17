2e66ff382628b444fecab8038fe79277a5cd5c9d
vishalk0002 <vishalk000123@gmail.com>
2024-10-18 04:24:02 +0530

    lc

---
+++ day_149/Find the minimum in rotated and sorted array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int s = 0;
+        int e  =  nums.size()-1;
+        while(s<e)
+        {
+            int mid =  s+(e-s)/2;
+            if(nums[mid] < nums[e])
+            {
+                e = mid;
+            }
+            else
+            {
+                s = mid+1;
+            }
+        }
+
+        return nums[s];
+    }
+};
+
+// leetcode -  153
