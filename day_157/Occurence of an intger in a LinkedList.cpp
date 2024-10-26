d41124b9ee02967ec436e411396774302cb44579
Vishal Kumar <vishalk000123@gmail.com>
2024-10-26 10:20:39 +0530

    gfg

---
+++ day_157/Occurence of an intger in a LinkedList.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+     int count(struct Node *head, int key)
+     {
+
+          int ans = 0;
+          while (head)
+          {
+               if (head->data == key)
+                    ans++;
+               head = head->next;
+          }
+
+          return ans;
+     }
+};
+
+// gfg
