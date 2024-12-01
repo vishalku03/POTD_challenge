63fabea5747f07a7a69ab7fda8423f1e14f4fd41
Vishal Kumar <vishalk000123@gmail.com>
2024-12-01 19:51:11 +0530

    lc

---
+++ day_187/1346. Check If N and Its Double Exist.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+     bool checkIfExist(vector<int> &arr)
+     {
+
+          int n = arr.size();
+          for (int i = 0; i < n; i++)
+          {
+               for (int j = i + 1; j < n; j++)
+               {
+                    if (2 * arr[i] == arr[j] || (arr[i] == 2 * arr[j]))
+                         return true;
+               }
+          }
+          return false;
+     }
+};
