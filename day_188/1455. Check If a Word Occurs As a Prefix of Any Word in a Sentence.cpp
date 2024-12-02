5ea59f6240bc2285ec0354c0dfa3113c12b0ee2f
Vishal Kumar <vishalk000123@gmail.com>
2024-12-02 21:43:46 +0530

    lc

---
+++ day_188/1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     int isPrefixOfWord(string sentence, string searchWord)
+     {
+          int i = 1, n = searchWord.size();
+          stringstream ans(sentence);
+          string word;
+          while (ans >> word)
+          {
+               if (word.substr(0, n) == searchWord)
+                    return i;
+               i++;
+          }
+          return -1;
+     }
+};
