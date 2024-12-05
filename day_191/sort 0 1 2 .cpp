5ce6b1a3b8c9c8df80608e82439ab60dd0c7401e
Vishal Kumar <vishalk000123@gmail.com>
2024-12-05 21:07:53 +0530

    gfg

---
+++ day_191/sort 0 1 2 .cpp
@@ -0,0 +1,13 @@
+
+class Solution
+{
+public:
+     void sort012(vector<int> &arr)
+     {
+          // code here
+          // int n = arr.size(),start = 0,end= n-1;
+          sort(arr.begin(), arr.end());
+     }
+};
+
+// gfg
