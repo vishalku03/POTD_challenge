b6903f138aa15673c5755629ebdc617858cc0a84
Vishal Kumar <vishalk000123@gmail.com>
2024-12-24 23:24:15 +0530

    gfg

---
+++ day_207/search in a sorted matrix.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+     // Function to search a given number in row-column sorted matrix.
+     bool searchMatrix(vector<vector<int>> &mat, int x)
+     {
+          int r = mat.size();
+          int c = mat[0].size();
+          int j = r * c - 1;
+          int i = 0;
+          while (i <= j)
+          {
+               int mid = i + (j - i) / 2;
+               int element = mat[mid / c][mid % c];
+               if (element == x)
+                    return true;
+               else if (element < x)
+                    i = mid + 1;
+               else
+                    j = mid - 1;
+          }
+          return false;
+     }
+};
+
+// gfg
